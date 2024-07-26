94eba09dbeb8a95931be7c21230dd86aa785a7cf
epicdigitalacademy <131464258+epicdigitalacademy@users.noreply.github.com>
2024-07-26 11:13:42 +0100

    Update wsgi.py

--- app/ddice_online_shop/wsgi.py
+++ app/ddice_online_shop/wsgi.py
@@ -12,5 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ddice_online_shop.settings')
-
+ALLOWED_HOSTS = ['*']
 application = get_wsgi_application()
